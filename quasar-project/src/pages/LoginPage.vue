72e96f9ea47e319056b925e9765841b8f039fb11
Vinicius Goncalves <vinigon01@gmail.com>
2025-11-16 14:46:48 -0300

    Ajuste na tela de login, prosseguir o login com a tecla "Enter".

--- quasar-project/src/pages/LoginPage.vue
+++ quasar-project/src/pages/LoginPage.vue
@@ -7,45 +7,47 @@
             Login do Sistema
           </q-card-section>
 
-          <q-card-section class="q-gutter-md">
-            <q-input
-              v-model="email"
-              label="E-mail"
-              type="email"
-              dense
-              outlined
-              autocomplete="email"
-            />
+          <q-form @submit.prevent="fazerLogin">
+            <q-card-section class="q-gutter-md">
+              <q-input
+                v-model="email"
+                label="E-mail"
+                type="email"
+                dense
+                outlined
+                autocomplete="email"
+              />
 
-            <q-input
-              v-model="senha"
-              label="Senha"
-              type="password"
-              dense
-              outlined
-              autocomplete="current-password"
-            />
+              <q-input
+                v-model="senha"
+                label="Senha"
+                type="password"
+                dense
+                outlined
+                autocomplete="current-password"
+              />
 
-            <q-banner
-              v-if="erroLogin"
-              class="q-mt-sm"
-              dense
-              rounded
-              color="negative"
-              text-color="white"
-            >
-              {{ erroLogin }}
-            </q-banner>
-          </q-card-section>
+              <q-banner
+                v-if="erroLogin"
+                class="q-mt-sm"
+                dense
+                rounded
+                color="negative"
+                text-color="white"
+              >
+                {{ erroLogin }}
+              </q-banner>
+            </q-card-section>
 
-          <q-card-actions align="right">
-            <q-btn
-              label="Entrar"
-              color="primary"
-              :loading="carregando"
-              @click="fazerLogin"
-            />
-          </q-card-actions>
+            <q-card-actions align="right">
+              <q-btn
+                label="Entrar"
+                color="primary"
+                :loading="carregando"
+                type="submit"  
+              />
+            </q-card-actions>
+          </q-form>
         </q-card>
       </q-page>
     </q-page-container>
@@ -64,7 +66,8 @@ import {
   QCardActions,
   QInput,
   QBanner,
-  QBtn
+  QBtn,
+  QForm
 } from 'quasar'
 
 const router = useRouter()
@@ -75,15 +78,17 @@ const erroLogin = ref(null)
 const carregando = ref(false)
 
 const fazerLogin = async () => {
+  if (carregando.value) return
+
   erroLogin.value = null
   carregando.value = true
 
   try {
-    const emailValido = 'admin@escola.com'
-    const senhaValida = '123456'
+    const emailValido = '123@gmail.com'
+    const senhaValida = '123'
 
     if (email.value === emailValido && senha.value === senhaValida) {
-      await router.push('/app') 
+      await router.push('/app')
     } else {
       erroLogin.value = 'E-mail ou senha inválidos.'
     }
